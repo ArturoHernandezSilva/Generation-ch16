059c76adec36dc3c1eacd9936bc2e98d0c50e879
Arturo <arturohdezslv@hotmail.com>
2022-08-16 16:31:31 -0500

    Java primera sesión 4:31pm 16 agosto

---
+++ 4-Java/ejemplojdk/Hola.java
@@ -0,0 +1,12 @@
+public class Hola {
+//esto es una clase del JDK Java
+String miVariable = "Hola mundo java"; 
+
+	public static void main (String arg []){
+	String miVariable = "Hola mundo java saludos a: ";
+	String miNombre = "Arturo";
+
+ 	//lo anterior es la declaracion del metodo main
+		System.out.println ("Hola saludos a " + miNombre);
+	}
+}
